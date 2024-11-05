42cfa81bcc15d26597f705348aceb33a2be19619
AzureAD\DavidDuarte <david.duarte@cross-join.com>
2024-11-05 15:50:50 +0000

    added test endpoint

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+	private final MicroserviceClient microserviceClient;
+
+	@Autowired
+	public HelloController(MicroserviceClient microserviceClient) {
+		this.microserviceClient = microserviceClient;
+	}
 
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
+	@PostMapping("/testEndpoint")
+	public String testEndpoint() {
+		return microserviceClient.getMicroserviceData();
+	}
 }
