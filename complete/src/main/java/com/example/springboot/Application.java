cf41927480fc0c6be41d4bc12cd97f630485d29b
AzureAD\DavidDuarte <david.duarte@cross-join.com>
2024-11-05 15:49:44 +0000

    added test endpoint

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -14,7 +15,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
---
+++ complete/src/main/java/com/example/springboot/MicroserviceClient.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MicroserviceClient {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MicroserviceClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getMicroserviceData() {
+        String url = "http://other-microservice/api/resource";  // Replace with your microservice URL
+        return restTemplate.getForObject(url, String.class);
+    }
+}
